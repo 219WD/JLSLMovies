2c397539118181740c4f5ef9d3e46dbf8de1c600
219WD <macgunmanmc@gmail.com>
2023-07-28 18:44:39 -0300

    actualizado el preload

--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,6 @@
 const preloader = document.getElementById("preload-container");
 const doneText = document.getElementById("doneText");
+gsap.from(".preload", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
 
 async function loadData() {
   preloader.style.display = "flex";
