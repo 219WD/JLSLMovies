704d9301a11e594781387673bca625c6b0ec6e7f
219WD <macgunmanmc@gmail.com>
2023-07-28 20:23:23 -0300

    preload premain completos login y register completos

---
+++ js/login.js
@@ -0,0 +1,84 @@
+//Perfil de ejemplo
+const admin = {
+    correo: "admin@admin.com",
+    password: "12345678",
+  };
+  
+  //Funcion logIn
+  
+  const logIn = (event) => {
+    event.preventDefault();
+  
+    //Obtengo los valores
+    let correo = document.querySelector("#emailLog").value;
+    let password = document.querySelector("#passwordLog").value;
+  
+    //Validar los valores
+    //   if(correo===admin.correo && password===admin.password)
+    if (correo === admin.correo) {
+      if (password === admin.password) {
+        //Enviamos al localS
+        localStorage.setItem("user", JSON.stringify(correo));
+  
+        //Ir a adminitracion
+        location.replace("/index.html");
+      }
+    } else {
+      alert("El correo o la contraseña es incoreecta!");
+    }
+  };
+  
+  //Escuchadores de eventos
+  document.getElementById("formLogIn").addEventListener("submit", logIn);
+  
+
+
+//-------------------------------------------------------------------------------------------
+//Register
+
+class Usuario {
+    constructor(id, usser, email, password) {
+        this.id = id;
+        this.usser = usser;
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  //RECIBIR DESDE LOCALSTORAGE
+  const data = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  
+//Funcion para agregar usuario nuevo
+
+const agregarUsuario = (event) => {
+    //1-Detener el submit
+    event.preventDefault();
+
+  
+    //3-crear id (sin usuarios preexistentes)
+    let id = new Date().getTime();
+  
+    //4-Capturar los datos de los input
+    let usser = document.querySelector("#username").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+    
+  
+    //5-Crear la clase usuario
+    let usuario = new Usuario(id, usser, email, password);
+  
+    //6-Agregar al arreglo el nuevo usuario
+    data.push(usuario);
+  
+    //!Agregar al localStorage
+    localStorage.setItem("usuarios", JSON.stringify(data));
+  
+    //7-Limpiar el form
+    document.querySelector("#usser").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#password").value = "";
+
+  
+  };
+  
