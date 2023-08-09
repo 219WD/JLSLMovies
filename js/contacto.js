d55fe2c9f1bf89278c7683ca11b4d20c92f321ad
Lucas Furmanski <lucasfurmanski408@gmail.com>
2023-08-09 10:58:07 -0300

    cambiando funciones para enviar y recibir correos

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,3 @@
-// capturar elementos del formulario apra pasarlo como parametro 
 function capturarDatos() {
     const nombre = document.getElementById("name").value;
     const correo = document.getElementById("email").value;
@@ -28,23 +27,57 @@ function capturarDatos() {
         return;
     }
   
-    enviarCorreo(correo, mensaje, nombre);
-  }
-    
-        
+    var templateParams = {
+      from_name: 'JLSLMovies',
+      user_name: nombre,
+      destinatario: correo,
+      message: 'Gracias por comunicarte con nosotros, en breve responderemos.',
+    };
   
-  
-  function enviarCorreo(correo, mensaje, nombre) {
-      Email.send({
-          Host : "smtp.elasticemail.com",
-          Username : "lfurmanski408@gmail.com",
-          Password : "12A95E1A3AABF42A1DDF38C15907B8E687EF",
-          To : correo,
-          From : "lfurmanski408@gmail.com",
-          Subject : "Mensaje de :" + nombre,  
-          Body : mensaje,
-      }).then(
-        message => alert(message)
-      );
+    enviarMailUsuario(templateParams);
+
+
+    var templateParamss = {
+        from_name: nombre,
+        user_name: 'JLSLMovies',
+        message: mensaje,
+      };
+
+      enviarMailAdm(templateParamss);
+
   }
+  
+  //envia un mail al usuario confirmando que se recibio su mensaje 
+  function enviarMailUsuario(params) {
+      emailjs.send('service_g7qe2am', 'template_z1n3yw7', params)
+      .then(function(response) {
+         console.log('SUCCESS!', response.status, response.text);
+         limpiarFormulario();
+      }, function(error) {
+         console.log('FAILED...', error);
+      });  
+  }
+//envia mai al administrador del sitio, con el mensaje del usuario 
+  function enviarMailAdm(paramss){
+    emailjs.send('service_g7qe2am', 'template_q9emwf7', paramss)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    }); 
+  }
+
+  function limpiarFormulario() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+
+    const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+    const messageError = document.getElementById("messageError");
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+}
   
