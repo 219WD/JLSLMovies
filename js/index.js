ed0e7ec2aa2f9c44dcf9ca6212e2631de8af38a9
219WD <macgunmanmc@gmail.com>
2023-08-09 00:26:29 -0300

    correccion z-index

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-
-
 gsap.from(".logo", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
 gsap.from(".nav-menu-item", {duration: 1, opacity: 0, scale: 0.3, y: -150});
 gsap.from(".txt-header", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
 gsap.from(".buttons", {duration: 1, opacity: 0, y: 150});
-
-
-
 
 const toggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-menu");
